d37df1aef5368138a56c4a8b20f021e9c5b906ce
YanaVivcharuk <yanavivcharuk@icloud.com>
2025-08-25 22:17:16 +0200

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import "./components/App/App.module.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./components/App/App";
+import App from "./components/App/App.tsx";
 import "modern-normalize/modern-normalize.css";
 
 const queryClient = new QueryClient();
@@ -13,9 +13,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
-
-const apiKey = import.meta.env.VITE_TMDB_TOKEN;
-
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
